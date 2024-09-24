f2f92dff6b3353b3b7a9f8ee13a6d602b35e56a2
KashifKhn <kashifkhnx04@gmail.com>
2024-09-24 09:42:25 +0500

    change folder structure

---
+++ lab1-principles/code/ReportGenerationSystem.java
@@ -0,0 +1,77 @@
+package task1.code;
+
+public class ReportGenerationSystem {
+  /**
+   * Tight or High Coupling
+   */
+
+  class PDFReport {
+    public void generate() {
+      System.out.println("Generating PDF Report...");
+    }
+  }
+
+  class ExcelReport {
+    public void generate() {
+      System.out.println("Generating Excel Report...");
+    }
+  }
+
+  class CSVReport {
+    public void generate() {
+      System.out.println("Generating CSV Report...");
+    }
+  }
+
+  class ReportService {
+    public void generatePDFReport() {
+      new PDFReport().generate();
+    }
+
+    public void generateExcelReport() {
+      new ExcelReport().generate();
+    }
+
+    public void generateCSVReport() {
+      new CSVReport().generate();
+    }
+  }
+
+  /**
+   * Loose or Low Coupling
+   */
+
+  interface ReportGenerator {
+    void generate();
+  }
+
+  class LCPDFReport implements ReportGenerator {
+    public void generate() {
+      System.out.println("Generating PDF Report...");
+    }
+  }
+
+  class LCExcelReport implements ReportGenerator {
+    public void generate() {
+      System.out.println("Generating Excel Report...");
+    }
+  }
+
+  class LCCSVReport implements ReportGenerator {
+    public void generate() {
+      System.out.println("Generating CSV Report...");
+    }
+  }
+
+  class LCReportService {
+    private ReportGenerator reportGenerator;
+
+    public LCReportService(ReportGenerator reportGenerator) {
+      this.reportGenerator = reportGenerator;
+    }
+
+    public void generateReport() {
+      reportGenerator.generate();
+    }
+  }
+}
